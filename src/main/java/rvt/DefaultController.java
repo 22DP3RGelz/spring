d7f49ff431221118c7076170499c640e8887342e
22DP3RGelz <131261269+22DP3RGelz@users.noreply.github.com>
2024-02-20 09:27:54 +0000

    AHAHA

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,14 +1,22 @@
 package rvt;
 
-// import java.util.ArrayList;
-// import java.util.Date;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -44,12 +52,44 @@ public class DefaultController {
 
     // int startIndex = 4;
         
-    @GetMapping(value = "/register")
-    public ModelAndView newregister(){
-        ModelAndView modelAndView = new ModelAndView("register");
-        return modelAndView;
-    }
+
+    @GetMapping(value = "/")
+
+        ModelAndView index(@RequestParam(name="name", required=false, defaultValue="null") String name) {
+            Student student = new Student("John", "Smith", "a@a.lv", "DP2-4");
+            List<Student> students = new ArrayList<>();
+            students.add(student);
+        
+
+        ModelAndView modelAndView = new ModelAndView("index");
+            modelAndView.addObject("students", students);
+            modelAndView.addObject("date", new Date().toString());
+            return modelAndView;
+        }
+
+        @GetMapping(value = "/register")
+        public ModelAndView test(@RequestParam HashMap<String, String> allParams, User user){
+
+        if(allParams.containsKey("success")){
+            ModelAndView modelAndView = new ModelAndView("success");
+            return modelAndView;
+        }
+            ModelAndView modelAndView = new ModelAndView("register");
+            return modelAndView;
+        }
+
+
+        @PostMapping(value = "/register")
+        public String test1(@ModelAttribute("user") User user, BindingResult bindingResult){
+
+            if (bindingResult.hasErrors()) {
+            return "/register";
+            }
+            return "redirect:/register?success";
+        }
+}
+
 
     
-}
+
 
